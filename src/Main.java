cd38cf3b858edb26daa1ddaf6418095ee9a76af5
Fortunate-Codes <adesinafortunate@mail.com>
2023-03-05 04:51:38 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number = input.nextInt();
+		
+		int sum = 0;
+		int digit;
+		
+		while(number != 0) {
+			digit = number % 10;
+			sum += digit;
+			number /= 10;
+		}
+		
+		System.out.println("The sum of digits is: " + sum);
+
+	}
+}
